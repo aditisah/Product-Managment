aa7ef171b893c78b5557712351694e56e0442654
Anand Sharma <sharmaanand570@gmail.com>
2022-07-27 18:17:57 +0530

    update

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -92,13 +92,67 @@ const createProduct = async function (req, res) {
     }
 }
 
-const getProducts = function (req, res) {
+const getProducts = async function (req, res) {
     try {
+        let productDetail = req.query;
+        let productFilter = { isDeleted: false, };
 
+        if (Object.keys(productDetail).includes('size')) {
+            if (productDetail.size.length == 0 || productDetail.size.trim() == '') {
+                res.status(400).send({ status: false, message: 'you selected the size field but value not provided' });
+                return;
+            }
+        }
+        if (Object.keys(productDetail).includes('name')) {
+            if (productDetail.name.length == 0 || productDetail.name.trim() == "''") {
+                res.status(400).send({ status: false, message: 'you selected the name field but value not provided' });
+                return;
+            }
+        }
+        if (Object.keys(productDetail).includes('priceLessThan')) {
+            if (productDetail.priceLessThan.length.trim() == 0) {
+                res.status(400).send({ status: false, message: 'you selected the priceLessThan field but value not provided' });
+                return;
+            }
+        }
+        if (Object.keys(productDetail).includes('priceGreaterThan')) {
+            if (productDetail.priceGreaterThan.trim().length == 0) {
+                res.status(400).send({ status: false, message: 'you selected the priceGreaterThan field but value not provided' });
+                return;
+            }
+        }
+        if (productDetail.size) {
+            productFilter.availableSizes = productDetail.size;
+        }
+        if (productDetail.name) {
+            productFilter.title = { $regex: productDetail.name, $options: "i" };
+        }
+        if (productDetail.priceLessThan) {
+            productFilter.price = { $lt: productDetail.priceLessThan };
+        }
+        if (productDetail.priceGreaterThan) {
+            if (productFilter.price) {
+                productFilter.price = {
+                    $lt: productDetail.priceLessThan,
+                    $gt: productDetail.priceGreaterThan,
+                };
+            } else {
+                productFilter.price = { $gt: productDetail.priceGreaterThan };
+            }
+        }
+        let filteredProduct = await productModel.find(productFilter).sort({ price: 1 });
+        if (filteredProduct.length == 0) {
+            res.status(404).send({ status: false, message: 'No product found!!' })
+            return
+        } else {
+            res.status(200).send({ status: true, message: "Success", data: filteredProduct });
+            return
+        }
+    } catch (err) {
+        res.status(500).send({ status: false, message: err.message });
     }
-    catch (err) {
-        res.status(500).send({ status: false, message: err.message })
-    }
-}
+};
+
+
 
 module.exports = { createProduct, getProducts };
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@ const { Route53RecoveryCluster } = require("aws-sdk");
 const express = require("express");
 const router = express.Router();
 const {updateUser, userLogin, register, getProfile} = require("../controllers/userController");
-const {createProduct} = require("../controllers/productController")
+const {createProduct, getProducts} = require("../controllers/productController")
 const {authentication} = require('../middleware/auth');
 
 //user routes
@@ -13,7 +13,7 @@ router.put("/user/:userId/profile", authentication, updateUser);
 
 //product routes
 router.post("/products", createProduct);
-
+router.get("/products", getProducts)
 //unknown route
 router.all('/*', function(res){
     res.status(400).send({status:false,message:"Invaild url"})

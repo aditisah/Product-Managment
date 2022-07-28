608b28969290d5358100cc0b2eb4c6d6b047f4b0
Anand Sharma <sharmaanand570@gmail.com>
2022-07-28 13:18:13 +0530

    Update

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,127 +1,129 @@
-const productModel = require("../models/productModel")
-const validator = require("../validator/validator")
-const awsService = require("../aws/config")
+const productModel = require("../models/productModel");
+const validator = require("../validator/validator");
+const awsService = require("../aws/config");
 
 const createProduct = async function (req, res) {
     try {
         if (Object.keys(req.body).length == 0) {
-            return res.status(400).send({ status: false, message: "Please Provide Necessary Details to create product" })
+            return res.status(400).send({ status: false, message: "Please Provide Necessary Details to create product", });
         }
-        let { title, description, price, currencyId, currencyFormat, isFreeShipping, style, availableSizes, installments } = req.body
+        let { title, description, price, currencyId, currencyFormat, isFreeShipping, style, availableSizes, installments } = req.body;
         if (!validator.isValid(title)) {
-            return res.status(400).send({ status: false, message: "please enter title, this field is mandatory" })
+            return res.status(400).send({ status: false, message: "please enter title, this field is mandatory" });
         }
-        const findTitle = await productModel.findOne({ title })
+        const findTitle = await productModel.findOne({ title });
         if (findTitle) {
-            return res.status(409).send({ status: false, message: "product title already present" })
+            return res.status(409).send({ status: false, message: "product title already present" });
         }
         if (!validator.isValid(description)) {
-            return res.status(400).send({ status: false, message: "please enter description, this field is mandatory" })
+            return res.status(400).send({ status: false, message: "please enter description, this field is mandatory" });
         }
         if (!validator.isValid(price) || !validator.isValidDecimalNumber(price)) {
-            return res.status(400).send({ status: false, message: "please enter valid price, this field is mandatory" })
+            return res.status(400).send({ status: false, message: "please enter valid price, this field is mandatory" });
         }
         if (!validator.isValid(currencyId) || currencyId.toUpperCase() != "INR") {
-            return res.status(400).send({ status: false, message: "please enter valid currencyId, this field is mandatory, e.g: INR" })
+            return res.status(400).send({ status: false, message: "please enter valid currencyId, this field is mandatory, e.g: INR" });
         }
         if (!validator.isValid(currencyFormat) || currencyFormat != "₹") {
-            return res.status(400).send({ status: false, message: "please enter valid currencyFormat, this field is mandatory, e.g: ₹" })
+            return res.status(400).send({ status: false, message: "please enter valid currencyFormat, this field is mandatory, e.g: ₹" });
         }
         if (!validator.isValid(availableSizes) || !validator.isValidSize(availableSizes)) {
-            return res.status(400).send({ status: false, message: "please enter valid availableSizes, at least on Size, e.g: M" })
+            return res.status(400).send({ status: false, message: "please enter valid availableSizes, at least on Size, e.g: M" });
         }
-        availableSizes = availableSizes.split(",").map(a => a.trim().toUpperCase())
-        availableSizes = [...new Set(availableSizes)]
+        availableSizes = availableSizes.split(",").map((a) => a.trim().toUpperCase());
+        availableSizes = [...new Set(availableSizes)];
 
-        let productData = { title, description, price, currencyId, currencyFormat, availableSizes }
+        let productData = { title, description, price, currencyId, currencyFormat, availableSizes };
 
         if (isFreeShipping) {
             if (isFreeShipping == "true") {
-                productData.isFreeShipping = true
+                productData.isFreeShipping = true;
             }
             else if (isFreeShipping == "false") {
-                productData.isFreeShipping = true
+                productData.isFreeShipping = true;
             }
             else {
-                return res.status(400).send({ status: false, message: "isFreeShipping should be true or false" })
+                return res.status(400).send({ status: false, message: "isFreeShipping should be true or false" });
             }
         }
         if (isFreeShipping == "") {
-            return res.status(400).send({ status: false, message: "you selected the isFreeShipping field but value not provided" })
+            return res.status(400).send({ status: false, message: "you selected the isFreeShipping field but value not provided" });
         }
         if (style) {
             if (!validator.isValid(style)) {
-                return res.status(400).send({ status: false, message: "please enter valid style" })
+                return res.status(400).send({ status: false, message: "please enter valid style" });
             }
-            productData.style = style
+            productData.style = style;
         }
         if (style == "") {
-            return res.status(400).send({ status: false, message: "you selected the style field but value not provided" })
+            return res.status(400).send({ status: false, message: "you selected the style field but value not provided" });
         }
         if (installments) {
             if (!validator.isValid(installments) || !validator.isValidDecimalNumber(installments)) {
-                return res.status(400).send({ status: false, message: "please enter valid installments" })
+                return res.status(400).send({ status: false, message: "please enter valid installments" });
             }
-            productData.installments = installments
+            productData.installments = installments;
         }
         if (installments == "") {
-            return res.status(400).send({ status: false, message: "you selected the installments field but value not provided" })
+            return res.status(400).send({ status: false, message: "you selected the installments field but value not provided" });
         }
 
         //product image
         const files = req.files;
         if (files.length == 0) {
-            return res.status(400).send({ status: false, message: "please provide profile Image" })
+            return res.status(400).send({ status: false, message: "please provide profile Image" });
         }
         if (!validator.isValidImage(files[0].originalname.toLowerCase())) {
-            return res.status(400).send({ status: false, message: "Image format is not correct" })
+            return res.status(400).send({ status: false, message: "Image format is not correct" });
         }
         if (files && files.length > 0 && files.length < 2) {
-            const uploadproductImage = await awsService.uploadImage(files[0])
-            productData.productImage = uploadproductImage
+            const uploadproductImage = await awsService.uploadImage(files[0]);
+            productData.productImage = uploadproductImage;
         }
         else {
-            return res.status(400).send({ status: false, message: "please provide only one product Image" })
+            return res.status(400).send({ status: false, message: "please provide only one product Image" });
         }
 
-        const createProduct = await productModel.create(productData)
-        return res.status(201).send({ status: true, message: "product data create sucessfully", data: createProduct })
-    } catch (err) {
-        res.status(500).send({ status: false, message: err.message })
+        const createProduct = await productModel.create(productData);
+        return res.status(201).send({ status: true, message: "product data create sucessfully", data: createProduct });
     }
-}
+    catch (err) {
+        res.status(500).send({ status: false, message: err.message });
+    }
+};
 
 const getProducts = async function (req, res) {
     try {
         let productDetail = req.query;
-        let productFilter = { isDeleted: false, };
+        let productFilter = { isDeleted: false };
 
-        if (Object.keys(productDetail).includes('size')) {
-            if (productDetail.size.length == 0 || productDetail.size.trim() == '') {
-                res.status(400).send({ status: false, message: 'you selected the size field but value not provided' });
+        if (Object.keys(productDetail).includes("size")) {
+            if (!validator.isValid(productDetail.size)) {
+                res.status(400).send({ status: false, message: "you selected the size field but value not provided", });
                 return;
             }
         }
-        if (Object.keys(productDetail).includes('name')) {
-            if (productDetail.name.length == 0 || productDetail.name.trim() == "''") {
-                res.status(400).send({ status: false, message: 'you selected the name field but value not provided' });
+        if (Object.keys(productDetail).includes("name")) {
+            if (!validator.isValid(productDetail.name)) {
+                res.status(400).send({ status: false, message: "you selected the name field but value not provided", });
                 return;
             }
         }
-        if (Object.keys(productDetail).includes('priceLessThan')) {
-            if (productDetail.priceLessThan.trim().length == 0) {
-                res.status(400).send({ status: false, message: 'you selected the priceLessThan field but value not provided' });
+        if (Object.keys(productDetail).includes("priceLessThan")) {
+            if (!validator.isValid(productDetail.priceLessThan)) {
+                res.status(400).send({ status: false, message: "you selected the priceLessThan field but value not provided", });
                 return;
             }
         }
-        if (Object.keys(productDetail).includes('priceGreaterThan')) {
-            if (productDetail.priceGreaterThan.trim().length == 0) {
-                res.status(400).send({ status: false, message: 'you selected the priceGreaterThan field but value not provided' });
+        if (Object.keys(productDetail).includes("priceGreaterThan")) {
+            if (!validator.isValid(productDetail.priceGreaterThan)) {
+                res.status(400).send({ status: false, message: "you selected the priceGreaterThan field but value not provided", });
                 return;
             }
         }
         if (productDetail.size) {
-            productFilter.availableSizes = productDetail.size;
+            let sizeArr = productDetail.size.split(",");
+            productFilter.availableSizes = { $in: sizeArr };
         }
         if (productDetail.name) {
             productFilter.title = { $regex: productDetail.name, $options: "i" };
@@ -135,26 +137,29 @@ const getProducts = async function (req, res) {
                     $lt: productDetail.priceLessThan,
                     $gt: productDetail.priceGreaterThan,
                 };
-            } else {
+            }
+            else {
                 productFilter.price = { $gt: productDetail.priceGreaterThan };
             }
         }
         let filteredProduct = await productModel.find(productFilter).sort({ price: 1 });
         if (filteredProduct.length == 0) {
-            res.status(404).send({ status: false, message: 'No product found!!' })
-            return
-        } else {
-            res.status(200).send({ status: true, message: "Success", data: filteredProduct });
-            return
+            res.status(404).send({ status: false, message: "No product found!!" });
+            return;
         }
-    } catch (err) {
+        else {
+            res.status(200).send({ status: true, message: "Success", data: filteredProduct });
+            return;
+        }
+    }
+    catch (err) {
         res.status(500).send({ status: false, message: err.message });
     }
 };
 
 const getProductbyId = async function (req, res) {
     try {
-        const productId = req.params.productId
+        const productId = req.params.productId;
         if (!productId) {
             return res.status(400).send({ status: false, message: "productId is required" });
         }
@@ -162,18 +167,17 @@ const getProductbyId = async function (req, res) {
             return res.status(400).send({ status: false, message: "productId is not valid" });
         }
 
-        const getproduct = await productModel.findOne({ _id: productId, isDeleted: false });
+        const getproduct = await productModel.findOne({ _id: productId, isDeleted: false, });
 
         if (!getproduct) {
-            return res.status(404).send({ status: false, message: "product not found" })
+            return res.status(404).send({ status: false, message: "product not found" });
         }
-        return res.status(200).send({ status: true, message: "product details", data: getproduct })
+        return res.status(200).send({ status: true, message: "product details", data: getproduct });
     }
-
     catch (err) {
-        res.status(500).send({ status: false, message: err.message })
+        res.status(500).send({ status: false, message: err.message });
     }
-}
+};
 
 const updateProduct = async (req, res) => {
     try {
@@ -181,7 +185,7 @@ const updateProduct = async (req, res) => {
         if (!validator.isValidObjectId(productId)) {
             return res.status(400).send({ status: false, message: "Invalid productId" })
         }
-        const findProduct = await productModel.findOne({_id:productId, isDeleted:false})
+        const findProduct = await productModel.findOne({ _id: productId, isDeleted: false })
         if (!findProduct) {
             return res.status(404).send({ status: false, message: "No product found" })
         }
@@ -254,7 +258,7 @@ const updateProduct = async (req, res) => {
             }
             availableSizes = availableSizes.trim().split(",").map(a => a.trim().toUpperCase())
             availableSizes = [...new Set(availableSizes)]
-            productData.$addToSet = { availableSizes: { $each :availableSizes} }
+            productData.$addToSet = { availableSizes: { $each: availableSizes } }
         }
         if (availableSizes == "") {
             return res.status(400).send({ status: false, message: "you selected the availableSizes field but value not provided" })
@@ -314,29 +318,27 @@ const updateProduct = async (req, res) => {
         return res.status(200).send({ status: true, message: "product data updated sucessfully", data: updatedProduct })
     }
     catch (err) {
-        res.status(500).send({ status: false, message: err.message });
+        return res.status(500).send({ status: false, message: err.message });
     }
 }
 
 const deleteProductbyId = async function (req, res) {
     try {
-        const productId = req.params.productId
+        const productId = req.params.productId;
         if (!productId) {
             return res.status(400).send({ status: false, message: "productId is required" });
         }
         if (!validator.isValidObjectId(productId)) {
             return res.status(400).send({ status: false, message: "productId is not valid" });
         }
-        const product = await productModel.findOneAndUpdate({ _id: productId, isDeleted: false }, { isDeleted: true, deletedAt: new Date() }, { new: true })
+        const product = await productModel.findOneAndUpdate({ _id: productId, isDeleted: false }, { isDeleted: true, deletedAt: new Date() }, { new: true });
         if (!product) {
-            return res.status(404).send({ status: true, message: "product not found " })
+            return res.status(404).send({ status: true, message: "product not found " });
         }
-        return res.status(200).send({ status: true, message: "product is deleted sucessfully" })
+        return res.status(200).send({ status: true, message: "product is deleted sucessfully" });
+    } catch (error) {
+        return res.status(500).send({ status: false, message: error.message });
     }
-
-    catch (error) {
-        return res.status(500).send({ status: false, message: error.message })
-    }
-}
+};
 
 module.exports = { createProduct, getProducts, getProductbyId, updateProduct, deleteProductbyId };

b7e8ba32d6bdcb4689129c7f3df67ea857fdbe42
Gaikawade <gmhs6199@gmail.com>
2022-07-28 08:02:12 +0530

    update product

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -90,10 +90,6 @@ const createProduct = async function (req, res) {
     }
 }
 
-
-
-
-
 const getProducts = async function (req, res) {
     try {
         let productDetail = req.query;
@@ -164,7 +160,8 @@ const getProducts = async function (req, res) {
     } catch (err) {
         res.status(500).send({ status: false, message: err.message });
     }
-};
+}
+
 const getProductbyId = async function (req, res) {
     try {
         const productId = req.params.productId
@@ -188,7 +185,98 @@ const getProductbyId = async function (req, res) {
     }
 }
 
+const updateProduct = async (req, res) => {
+    try{
+        let _id = req.params.productId;
+        if(isValidObjectId(_id)){
+            return res.status(400).send({ status: false, message: "ProductId is not valid" });
+        }
 
+        let { title, description, price, currencyId, currencyFormat, isFreeShipping, style, availableSizes, installments } = req.body
+        if(title){
+            if (!validator.isValid(title)) {
+                return res.status(400).send({ status: false, message: "please enter title, this field is mandatory" })
+            }
+            const findTitle = await productModel.findOne({ title })
+            if (findTitle) {
+                return res.status(409).send({ status: false, message: "product title already present" })
+            }
+        }
+        if(description){
+            if (!validator.isValid(description)) {
+                return res.status(400).send({ status: false, message: "please enter description, this field is mandatory" })
+            }
+        }
+        if(price){
+            if (!validator.isValid(price) || !validator.isValidDecimalNumber(price)) {
+                return res.status(400).send({ status: false, message: "please enter valid price, this field is mandatory" })
+            }
+        }
+        if(availableSizes){
+            if (!validator.isValid(availableSizes) || !validator.isValidSize(availableSizes)) {
+                return res.status(400).send({ status: false, message: "please enter valid availableSizes, at least on Size, e.g: M" })
+            }
+            availableSizes = availableSizes.split(",").map(a => a.trim().toUpperCase())
+        }
+
+        let productData = { title, description, price, availableSizes }
+
+        if (isFreeShipping) {
+            if (isFreeShipping == "true") {
+                productData.isFreeShipping = true
+            }
+            else if (isFreeShipping == "false") {
+                productData.isFreeShipping = true
+            }
+            else {
+                return res.status(400).send({ status: false, message: "isFreeShipping should be true or false" })
+            }
+        }
+        if (isFreeShipping == "") {
+            return res.status(400).send({ status: false, message: "you selected the isFreeShipping field but value not provided" })
+        }
+        if (style) {
+            if (!validator.isValid(description)) {
+                return res.status(400).send({ status: false, message: "please enter valid style" })
+            }
+            productData.style = style
+        }
+        if (style == "") {
+            return res.status(400).send({ status: false, message: "you selected the style field but value not provided" })
+        }
+        if (installments) {
+            if (!validator.isValid(installments) || !validator.isValidDecimalNumber(installments)) {
+                return res.status(400).send({ status: false, message: "please enter valid installments" })
+            }
+            productData.installments = installments
+        }
+        if (installments == "") {
+            return res.status(400).send({ status: false, message: "you selected the installments field but value not provided" })
+        }
+
+        //product image
+        const files = req.files;
+        if (files.length == 0) {
+            return res.status(400).send({ status: false, message: "please provide profile Image" })
+        }
+        if (!validator.isValidImage(files[0].originalname.toLowerCase())) {
+            return res.status(400).send({ status: false, message: "Image format is not correct" })
+        }
+        if (files && files.length > 0 && files.length < 2) {
+            const uploadproductImage = await awsService.uploadImage(files[0])
+            productData.productImage = uploadproductImage
+        }
+        else {
+            return res.status(400).send({ status: false, message: "please provide only one product Image" })
+        }
+
+        const updateedProduct = await productModel.findOneAndUpdate({_id}, {productData}, {new: true});
+        return res.status(201).send({ status: true, message: "product data updated sucessfully", data: updateedProduct })
+    }
+    catch(err){
+        res.status(500).send({ status: false, message: err.message });
+    }
+}
 
 const deleteProductbyId = async function (req, res) {
     try {
@@ -211,12 +299,7 @@ const deleteProductbyId = async function (req, res) {
     }
 }
 
-
-
-
-
-
-module.exports = { createProduct, getProducts, getProductbyId, deleteProductbyId };
+module.exports = { createProduct, getProducts, getProductbyId, updateProduct, deleteProductbyId };
 
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@ const { Route53RecoveryCluster } = require("aws-sdk");
 const express = require("express");
 const router = express.Router();
 const { updateUser, userLogin, register, getProfile } = require("../controllers/userController");
-const { createProduct, getProducts, getProductbyId, deleteProductbyId } = require("../controllers/productController");
+const { createProduct, getProducts, getProductbyId, deleteProductbyId, updateProduct } = require("../controllers/productController");
 const { authentication } = require('../middleware/auth');
 
 
@@ -18,7 +18,8 @@ router.put("/user/:userId/profile", authentication, updateUser);
 router.post("/products", createProduct);
 router.get('/products/:productId', getProductbyId);
 router.delete('/products/:productId', deleteProductbyId);
-router.get("/products", getProducts)
+router.get("/products", getProducts);
+router.update('/products/:productId', updateProduct);
 
 //=================unknown route===================================//
 router.all('/*', function (res) {
